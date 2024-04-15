e13ebd4939d148784d5f7fc6f0427c6165dd39ab
Steve <ramiandrisoasteve@gmail.com>
2024-04-15 15:09:29 +0300

    creation table dans la base de donnée après run

---
+++ src/main/java/com/tpjakarta/tpbanque/mg/itu/steve/service/GestionnaireCompte.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.tpjakarta.tpbanque.mg.itu.steve.service;
+
+import com.tpjakarta.tpbanque.mg.itu.steve.entity.CompteBancaire;
+import jakarta.annotation.sql.DataSourceDefinition;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Query;
+import jakarta.transaction.Transactional;
+import java.util.List;
+
+/**
+ *
+ * @author Steve
+ */
+@Named(value = "gestionnaireCompte")
+@DataSourceDefinition (
+    className="com.mysql.cj.jdbc.MysqlDataSource",
+    name="java:app/jdbc/banque",
+    serverName="localhost",
+    portNumber=3306,
+    user="root",              // nom et
+    password="root", // mot de passe que vous avez donnés lors de la création de la base de données
+    databaseName="banque",
+    properties = {
+      "useSSL=false",
+      "allowPublicKeyRetrieval=true",
+      "driverClass=com.mysql.cj.jdbc.Driver"
+    }
+)
+@RequestScoped
+public class GestionnaireCompte {
+    
+    @PersistenceContext(unitName = "banquePU")
+    private EntityManager em;
+
+    /**
+     * Creates a new instance of GestionnaireCompte
+     */
+    public GestionnaireCompte() {
+    }
+    
+    @Transactional
+    public void creerCompte(CompteBancaire c) {
+        em.persist(c);
+    }
+    
+    public List<CompteBancaire> getAllComptes() {
+       Query query = em.createNamedQuery("CompteBancaire.findAll");
+       return query.getResultList();
+    }
+    
+}
