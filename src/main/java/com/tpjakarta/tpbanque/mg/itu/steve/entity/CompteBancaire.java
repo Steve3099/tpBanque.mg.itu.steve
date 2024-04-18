4dcfb29747d7afb81eb8bffa6925c38307d27561
Steve <ramiandrisoasteve@gmail.com>
2024-04-18 15:08:10 +0300

    version

--- src/main/java/com/tpjakarta/tpbanque/mg/itu/steve/entity/CompteBancaire.java
+++ src/main/java/com/tpjakarta/tpbanque/mg/itu/steve/entity/CompteBancaire.java
@@ -14,6 +14,7 @@ import jakarta.persistence.NamedQueries;
 import jakarta.persistence.NamedQuery;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.Version;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -37,6 +38,8 @@ public class CompteBancaire implements Serializable {
 
     private int solde;
 
+    @Version
+    private int version;
 
     @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<OperationBancaire> operations = new ArrayList<>();
